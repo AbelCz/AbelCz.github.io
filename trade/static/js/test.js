9a4228706a5e12279887c009270819b22a455b23
rainbowfirestudios <rainbowfirestudios@gmail.com>
2022-06-27 18:01:21 -0400

    Update test.js

---
+++ trade/static/js/test.js
@@ -0,0 +1,20 @@
+function fetchStatus() {
+    fetch('https://api137.radioshack.org/api/tokens/0x613a489785C95afEB3b404CC41565cCff107B6E0')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        appendDataAsTable(data);
+      })
+      .catch(function (err) {
+        console.log('error: ' + err);
+      });
+  }
+  
+  window.addEventListener('load', function () {
+    // Your document is loaded.
+    var fetchInterval = 5000; // 5 seconds.
+  
+    // Invoke the request every 5 seconds.
+    setInterval(fetchStatus, fetchInterval);
+  });
