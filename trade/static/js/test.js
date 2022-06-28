795d0edacb720258beccc6035d3124455b8f0ab7
rainbowfirestudios <rainbowfirestudios@gmail.com>
2022-06-27 20:32:01 -0400

    Update test.js

--- trade/static/js/test.js
+++ trade/static/js/test.js
@@ -5,10 +5,12 @@ function fetchStatus() {
     fetch('https://api137.radioshack.org/api/tokens/0x613a489785C95afEB3b404CC41565cCff107B6E0')
       .then(function (response) {
         return response.json();
-        console.log(data);
       })
       .then(function (data) {
         appendDataAsTable(data);
+      })
+      .then(function (data){
+        tokenDetails(data);
       })
       .catch(function (err) {
         console.log('error: ' + err);
@@ -27,12 +29,6 @@ function fetchStatus() {
   function appendDataAsTable(data) {
     var oldPrice = localStorage.getItem(data.data.symbol+'price');
     tbl = document.getElementById('pricerealtime');
-    fullnameoftoken = document.getElementById('_3TcfXW9zOvs5Y857gZVxgv');
-    fullnameoftoken2 = document.getElementById('_1WwkcxTzl-09aDCpRhYU1u');
-    symboloftoken = document.getElementById('_3Bx-FZIaBbVKssHb-LYGou');
-    symboloftoken2 = document.getElementById('_38idpiULmwLdV8ZLER8eXp _2p9QAAMCqbu8kWNa-OxhxO aZJzIkLG4sRsoY7PeO5H6');
-    symboloftoken3 = document.getElementById('_38idpiULmwLdV8ZLER8eXp _2p9QAAMCqbu8kWNa-OxhxO _1Io-xGeWSmRodp84w_CceQ');
-    symboloftoken4 = document.getElementById('_38idpiULmwLdV8ZLER8eXp _2p9QAAMCqbu8kWNa-OxhxO aZJzIkLG4sRsoY7PeO5H6');
     localStorage.setItem(data.data.symbol+'price', data.data.price);
     var currentPrice = localStorage.getItem(data.data.symbol+'price');
     asd = data.data.price.substring(0, 7);
@@ -50,10 +46,20 @@ function fetchStatus() {
     }
     // Overwrite the existing HTML with new content received.
     tbl.innerHTML = dsa;
+}
+
+function tokenDetails(data){
+  fullnameoftoken = document.getElementById('_3TcfXW9zOvs5Y857gZVxgv');
+    fullnameoftoken2 = document.getElementById('_1WwkcxTzl-09aDCpRhYU1u');
+    symboloftoken = document.getElementById('_3Bx-FZIaBbVKssHb-LYGou');
+    symboloftoken2 = document.getElementById('_38idpiULmwLdV8ZLER8eXp _2p9QAAMCqbu8kWNa-OxhxO aZJzIkLG4sRsoY7PeO5H6');
+    symboloftoken3 = document.getElementById('_38idpiULmwLdV8ZLER8eXp _2p9QAAMCqbu8kWNa-OxhxO _1Io-xGeWSmRodp84w_CceQ');
+    symboloftoken4 = document.getElementById('_38idpiULmwLdV8ZLER8eXp _2p9QAAMCqbu8kWNa-OxhxO aZJzIkLG4sRsoY7PeO5H6');
     fullnameoftoken.innerHTML = data.data.fullnameoftoken;
     fullnameoftoken2.innerHTML = 'Connect your ' + data.data.fullnameoftoken + ' wallet to deposit funds & start trading.';
     symboloftoken.innerHTML = data.data.symbol;
     symboloftoken2.innerHTML = data.data.symbol;
     symboloftoken3.innerHTML = data.data.symbol;
     symboloftoken4.innerHTML = data.data.symbol;
+
 }
