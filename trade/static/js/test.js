d7cd7234d4abc5c14554963346b7d30c7f457f6f
rainbowfirestudios <rainbowfirestudios@gmail.com>
2022-06-28 03:22:03 -0400

    Update test.js

--- trade/static/js/test.js
+++ trade/static/js/test.js
@@ -180,7 +180,7 @@ function fetchTokenlist(){
       base_symbols = data2.data[keyNames[zozo]].base_symbol;
       base_names = data2.data[keyNames[zozo]].base_name;
       quote_symbols = data2.data[keyNames[zozo]].quote_symbol;
-      url_logos = "https://abelcz.github.io/currencies/"+toLowerCase(base_symbols)+".svg"
+      url_logos = "https://abelcz.github.io/currencies/"+base_symbols.toLowerCase+".svg"
       prices = data2.data[keyNames[zozo]].price;
       base_addresses = data2.data[keyNames[zozo]].base_address;
       quote_addresses = data2.data[keyNames[zozo]].quote_address;
