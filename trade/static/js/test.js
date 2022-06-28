68c4f89fc3636f16982feeafa52fad70a863e67d
rainbowfirestudios <rainbowfirestudios@gmail.com>
2022-06-28 01:18:30 -0400

    Update test.js

--- trade/static/js/test.js
+++ trade/static/js/test.js
@@ -176,48 +176,59 @@ function fetchTokenlist(){
 
     for (let i = 0; i < totalpairs; i++) {
       //eth button
+    var m = "m"+[i;
     const m = document.createElement('div');
     m.classList.add("_1i_jfKy_CR-3JEUUoLyTyo");
     m.setAttribute("role","button");
     document.getElementsByClassName("_6_s1igBgOZG_l67nN_HjG")[i].appendChild(m);
-
+    
+    var n = "n"+[i];
     const n = document.createElement('div');
     n.classList.add("_2jve0XRK1SS7xTFWQHU7sV");
     document.getElementsByClassName("_1i_jfKy_CR-3JEUUoLyTyo")[i].appendChild(n);
 
+    var o = "o"+[i];
     const o = document.createElement('div');
     o.classList.add("_3703-MxhX16pYBOSod3SpG");
     document.getElementsByClassName("_2jve0XRK1SS7xTFWQHU7sV")[i].appendChild(o);
 
+    var p = "p"+[i];
     const p = document.createElement('div');
     p.classList.add("_1WIhq_BSoc6G4Z_9FtHB63","_242klziznfQnEkLJuh1ZoS");
     document.getElementsByClassName("_3703-MxhX16pYBOSod3SpG")[i].appendChild(p);
   //add image
+    var q = "q"+[i];
     const q = document.createElement('img');
     q.src = "/currencies/eth.svg";
     document.getElementsByClassName("_1WIhq_BSoc6G4Z_9FtHB63","_242klziznfQnEkLJuh1ZoS")[i].appendChild(q);
   //add name
+    var r = "r"+[i];
     const r = document.createElement('span');
     r.innerHTML = "Ethereum";
     document.getElementsByClassName("_3703-MxhX16pYBOSod3SpG")[i].appendChild(r);
   //add ticker
+    var s = "s"+[i];
     const s = document.createElement('span');
     s.classList.add("_38idpiULmwLdV8ZLER8eXp","aZJzIkLG4sRsoY7PeO5H6");
     s.innerHTML = "ETH";
     document.getElementsByClassName("_3703-MxhX16pYBOSod3SpG")[i].appendChild(s);
 
+    var t = "t"+[i];
     const t = document.createElement('div');
     t.classList.add("_2d-dptgdt6bp7FHSPkHbT6");
     document.getElementsByClassName("_2jve0XRK1SS7xTFWQHU7sV")[i].appendChild(t);
   // price here
+    var u = "u"+[i];
     const u = document.createElement('span');
     u.innerHTML = "price here";
     document.getElementsByClassName("_2d-dptgdt6bp7FHSPkHbT6")[i].appendChild(u);
 
+    var v = "v"+[i];
     const v = document.createElement('div');
     v.classList.add("_1spo-4DNNHD_uNqIyedTNs","_3CAUNSCsacK-I2JgikLL4R");
     document.getElementsByClassName("_2d-dptgdt6bp7FHSPkHbT6")[i].appendChild(v);
   //price change
+    var w = "w"+[i];
     const w = document.createElement('span');
     w.innerHTML = "percentage";
     document.getElementsByClassName("_1spo-4DNNHD_uNqIyedTNs","_3CAUNSCsacK-I2JgikLL4R")[i].appendChild(w);
