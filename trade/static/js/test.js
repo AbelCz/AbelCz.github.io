a1bca6671360b4454e593aeb457e0d7369c84e75
rainbowfirestudios <rainbowfirestudios@gmail.com>
2022-06-27 23:25:42 -0400

    dsa

--- trade/static/js/test.js
+++ trade/static/js/test.js
@@ -93,6 +93,66 @@ function switcher() {
   i.innerHTML = '<svg width="6" height="5" viewBox="0 0 6 5" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.6038 4.48532C2.80395 4.74532 3.19605 4.74532 3.3962 4.48532L5.84441 1.305C6.09751 0.976213 5.86313 0.5 5.44821 0.5H0.551788C0.136868 0.5 -0.0975129 0.976213 0.155586 1.305L2.6038 4.48532Z" fill="#6f6e84"></path></svg>';
   document.getElementsByClassName("AZo34VCmA4oZOJ3Yp3mqn._2JsEUUssaQhHp3oz89-xZs")[1].appendChild(i);
 
+  const j = document.createElement('div');
+  j.classList.add("GV27ba5q8OKFWJJOAIZpH");
+  document.getElementsByClassName("_1YMMq6xZbYvcxQOeLv5m-t")[0].appendChild(j);
 
+  const k = document.createElement('div');
+  k.classList.add("_14rwWgDcatef27EFjPdYOM");
+  document.getElementsByClassName("GV27ba5q8OKFWJJOAIZpH-t")[0].appendChild(k);
+
+  const l = document.createElement('div');
+  l.classList.add("_6_s1igBgOZG_l67nN_HjG");
+  document.getElementsByClassName("_14rwWgDcatef27EFjPdYOM-t")[0].appendChild(l);
+//eth button
+  const m = document.createElement('div');
+  m.classList.add("_1i_jfKy_CR-3JEUUoLyTyo._1uC2xzVGl7L8alfssRyH8r");
+  m.setAttribute("role","button");
+  document.getElementsByClassName("_6_s1igBgOZG_l67nN_HjG-t")[0].appendChild(m);
+
+  const n = document.createElement('div');
+  n.classList.add("_2jve0XRK1SS7xTFWQHU7sV");
+  document.getElementsByClassName("_1i_jfKy_CR-3JEUUoLyTyo._1uC2xzVGl7L8alfssRyH8r")[0].appendChild(n);
+
+  const o = document.createElement('div');
+  o.classList.add("_3703-MxhX16pYBOSod3SpG");
+  document.getElementsByClassName("_2jve0XRK1SS7xTFWQHU7sV")[0].appendChild(o);
+
+  const p = document.createElement('div');
+  p.classList.add("_1WIhq_BSoc6G4Z_9FtHB63._242klziznfQnEkLJuh1ZoS");
+  document.getElementsByClassName("_3703-MxhX16pYBOSod3SpG")[0].appendChild(p);
+
+  const q = document.createElement('img');
+  q.src = "/currencies/eth.svg";
+  document.getElementsByClassName("_1WIhq_BSoc6G4Z_9FtHB63._242klziznfQnEkLJuh1ZoS")[0].appendChild(q);
+
+  const r = document.createElement('span');
+  r.innerHTML = "Ethereum";
+  document.getElementsByClassName("_3703-MxhX16pYBOSod3SpG")[0].appendChild(r);
+
+  const s = document.createElement('span');
+  s.classList.add("_38idpiULmwLdV8ZLER8eXp.aZJzIkLG4sRsoY7PeO5H6");
+  s.innerHTML = "ETH";
+  document.getElementsByClassName("_3703-MxhX16pYBOSod3SpG")[0].appendChild(s);
+
+  const t = document.createElement('div');
+  t.classList.add("_2d-dptgdt6bp7FHSPkHbT6");
+  document.getElementsByClassName("_2jve0XRK1SS7xTFWQHU7sV")[0].appendChild(t);
+
+  const u = document.createElement('span');
+  u.innerHTML = "price here";
+  document.getElementsByClassName("_2d-dptgdt6bp7FHSPkHbT6")[0].appendChild(u);
+
+  const v = document.createElement('div');
+  v.classList.add("_1spo-4DNNHD_uNqIyedTNs _3CAUNSCsacK-I2JgikLL4R");
+  document.getElementsByClassName("_2d-dptgdt6bp7FHSPkHbT6")[0].appendChild(v);
+
+  const w = document.createElement('span');
+  w.innerHTML = "percentage"
+  document.getElementsByClassName("_1spo-4DNNHD_uNqIyedTNs _3CAUNSCsacK-I2JgikLL4R")[0].appendChild(w);
+
+
+
+  GV27ba5q8OKFWJJOAIZpH 
   fetch('https://api137.radioshack.org/api/pairs');
 }
