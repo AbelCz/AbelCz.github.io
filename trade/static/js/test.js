4fc87c030b093bfbc833dc425384083ce6706395
rainbowfirestudios <rainbowfirestudios@gmail.com>
2022-06-28 22:07:17 -0400

    Update test.js

--- trade/static/js/test.js
+++ trade/static/js/test.js
@@ -364,8 +364,8 @@ function fetchPreviousTrades(){
           })
     }
     function PreviousTrades(data6){
-      var totaltrades = Object.keys(data6.arr.tokenAddressIn).length;
-      let keyTokensIn = Object.keys(data6.arr.tokenAddressIn);
+      var totaltrades = Object.keys(data6.tokenAddressIn).length;
+      let keyTokensIn = Object.keys(data6.tokenAddressIn);
       console.log("total trades " + totaltrades);
       console.log(data6.data);
   
